aa46c0dea86d0fda7fd2d0ad7a1a5734a13b25de
MSI-PC <MSI-PC@MSI>
2024-07-17 00:43:33 +0300

    Add project files.

---
+++ GreetingsApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Welcome to the Greeting App.");
+        }
+    }
+}
